c495ff6e0e3a6a8da56f6ac414cdda908809b124
clev <clev@clev.local>
2016-04-12 22:04:38 -0500

    added date

--- kevtut.py
+++ kevtut.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template
+import datetime
 kevtut = Flask(__name__)
+
+today = datetime.date.today()
 
 @kevtut.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', date=today)
 
 if __name__ == '__main__':
     kevtut.run(debug=True)

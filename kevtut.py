835a16022e7f7eb4f676d93aa873f00e9bec2b73
clev <clev@clev.local>
2016-04-12 22:39:40 -0500

    got the get request working to dump to the page with no formatting or a specific date selection

--- kevtut.py
+++ kevtut.py
@@ -1,12 +1,18 @@
 from flask import Flask,render_template
 import datetime
+import requests
+
 kevtut = Flask(__name__)
 
+# setting some variables
 today = datetime.date.today()
+url = 'http://saallergy.info/'
+headers = {'Accept': 'application/json'}
+r = requests.get(url, headers=headers)
 
 @kevtut.route('/')
 def index():
-    return render_template('index.html', date=today)
+    return render_template('index.html', date=today, get=r.json())
 
 if __name__ == '__main__':
     kevtut.run(debug=True)
